1df6475554b4c41b5152586f96e2e4a30510d2e6
mouse <winne2006@gmail.com>
2026-02-14 12:38:15 +0800

    add prproducts.py

---
+++ products.py
@@ -0,0 +1,16 @@
+
+products = []
+
+while True :
+	name = input ("請輸入商品名稱: ")
+	if name == "q" or name == "Q" : 
+		break
+	price = input("請輸入價格: ")
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	p = [name , price]
+	products.append(p)
+print(products)
+
+
